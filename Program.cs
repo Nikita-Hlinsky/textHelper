5b73f840a9b966eae6b20e255cf37f395a205c2d
Niki <super.nikita0308@gmail.com>
2019-01-09 13:07:51 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4
+{   public class StringHelper
+    {
+        public static string TurnOn(string s, int max)
+        {
+            if(s == null)
+            {
+                return string.Empty;
+            }
+            if(s.Length <= max)
+            {
+                return s;
+            }
+            return s.Substring(0, max) + "...";
+        }       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Очень длинная строка коротая резко закончится в любом месте";
+            Console.WriteLine(StringHelper.TurnOn(s, 20));
+            
+        }
+    }
+}
